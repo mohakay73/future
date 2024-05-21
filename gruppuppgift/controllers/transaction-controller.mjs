e80781381fb848279bf2e579e5d51f579432b20f
El-Duckster <144563447+El-Duckster@users.noreply.github.com>
2024-05-21 14:27:20 +0200

    lagt till transaction hantering

---
+++ gruppuppgift/controllers/transaction-controller.mjs
@@ -0,0 +1,43 @@
+import { blockchain } from '../startup.mjs';
+
+export const createTransaction = (req, res) => {
+  const transaction = req.body;
+
+  const blockIndex = blockchain.addTransaction(transaction);
+
+  res.status(201).json({
+    success: true,
+    statusCode: 201,
+    data: { message: 'Transaktion skapad', transaction, blockIndex },
+  });
+};
+
+export const broadcastTransaction = (req, res) => {
+  const transaction = blockchain.createTransaction(
+    req.body.amount,
+    req.body.sender,
+    req.body.recipient
+  );
+
+  const blockIndex = blockchain.addTransaction(transaction);
+
+  blockchain.memberNodes.forEach(async (url) => {
+    await fetch(`${url}/api/v1/transactions/transaction`, {
+      method: 'POST',
+      body: JSON.stringify(transaction),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  });
+
+  res.status(201).json({
+    success: true,
+    statusCode: 201,
+    data: {
+      message: 'Transaktion skapad och distribuerad',
+      transaction,
+      blockIndex,
+    },
+  });
+};
